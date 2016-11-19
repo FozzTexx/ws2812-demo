eebb8868672e77566e8745f41edf146bcab41b1d
Chris Osborn <fozztexx@fozztexx.com>
2016-11-19 10:05:05 -0800

    Initial version.

---
+++ main/main.c
@@ -0,0 +1,97 @@
+/* Created 19 Nov 2016 by Chris Osborn <fozztexx@fozztexx.com>
+ * http://insentricity.com
+ *
+ * Demo of driving WS2812 RGB LEDs using the RMT peripheral.
+ *
+ * This code is placed in the public domain.
+ */
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <soc/rmt_struct.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <driver/gpio.h>
+#include <stdio.h>
+#include "ws2812.h"
+
+#define WS2812_PIN	18
+
+#define delay_ms(ms) vTaskDelay((ms) / portTICK_RATE_MS)
+
+void rainbow(void *pvParameters)
+{
+  const uint8_t anim_step = 10;
+  const uint8_t anim_max = 250;
+  const uint8_t pixel_count = 9; // Number of your "pixels"
+  const uint8_t delay = 25; // duration between color changes
+  rgbVal color = makeRGBVal(anim_max, 0, 0);
+  uint8_t step = 0;
+  rgbVal color2 = makeRGBVal(anim_max, 0, 0);
+  uint8_t step2 = 0;
+  rgbVal *pixels;
+
+
+  pixels = malloc(sizeof(rgbVal) * pixel_count);
+  
+  while (1) {
+    color = color2;
+    step = step2;
+
+    for (uint8_t i = 0; i < pixel_count; i++) {
+      pixels[i] = color;
+
+      if (i == 1) {
+        color2 = color;
+        step2 = step;
+      }
+
+      switch (step) {
+      case 0:
+        color.g += anim_step;
+        if (color.g >= anim_max)
+          step++;
+        break;
+      case 1:
+        color.r -= anim_step;
+        if (color.r == 0)
+          step++;
+        break;
+      case 2:
+        color.b += anim_step;
+        if (color.b >= anim_max)
+          step++;
+        break;
+      case 3:
+        color.g -= anim_step;
+        if (color.g == 0)
+          step++;
+        break;
+      case 4:
+        color.r += anim_step;
+        if (color.r >= anim_max)
+          step++;
+        break;
+      case 5:
+        color.b -= anim_step;
+        if (color.b == 0)
+          step = 0;
+        break;
+      }
+    }
+
+    ws2812_setColors(pixel_count, pixels);
+    
+    delay_ms(delay);
+  }
+}
+
+void app_main()
+{
+  nvs_flash_init();
+
+  ws2812_init(WS2812_PIN);
+  xTaskCreate(rainbow, "ws2812 rainbow demo", 256, NULL, 10, NULL);
+
+  return;
+}
---
+++ main/ws2812.c
@@ -0,0 +1,174 @@
+/* Created 19 Nov 2016 by Chris Osborn <fozztexx@fozztexx.com>
+ * http://insentricity.com
+ *
+ * Uses the RMT peripheral on the ESP32 for very accurate timing of
+ * signals sent to the WS2812 LEDs.
+ *
+ * This code is placed in the public domain.
+ *
+ * For timing information check:
+ * https://cpldcpu.wordpress.com/2014/01/14/light_ws2812-library-v2-0-part-i-understanding-the-ws2812/
+ */
+
+#include "ws2812.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <soc/rmt_struct.h>
+#include <soc/dport_reg.h>
+#include <driver/gpio.h>
+#include <soc/gpio_sig_map.h>
+#include <esp_intr.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ETS_RMT_CTRL_INUM	18
+#define ESP_RMT_CTRL_DISABLE	ESP_RMT_CTRL_DIABLE /* Typo in esp_intr.h */
+
+#define WS2812_CYCLE	225 /* nanoseconds */
+#define RESET		50000 /* nanoseconds */
+#define DURATION	12.5 /* minimum time of a single RMT duration
+				in nanoseconds based on clock */
+#define DIVIDER		1 /* Any other values cause flickering */
+#define PULSE		((WS2812_CYCLE * 2) / (DURATION * DIVIDER))
+#define MAX_PULSES	32
+
+#define RMTCHANNEL	0
+
+typedef union {
+  struct {
+    uint32_t duration0:15;
+    uint32_t level0:1;
+    uint32_t duration1:15;
+    uint32_t level1:1;
+  };
+  uint32_t val;
+} rmtPulsePair;
+
+static uint8_t *ws2812_buffer = NULL;
+static unsigned int ws2812_pos, ws2812_len, ws2812_half;
+static xSemaphoreHandle ws2812_sem;
+static rmtPulsePair ws2812_bits[2];
+
+void ws2812_initRMTChannel(int rmtChannel)
+{
+  RMT.apb_conf.fifo_mask = 1;  //enable memory access, instead of FIFO mode.
+  RMT.apb_conf.mem_tx_wrap_en = 1; //wrap around when hitting end of buffer
+  RMT.conf_ch[rmtChannel].conf0.div_cnt = DIVIDER;
+  RMT.conf_ch[rmtChannel].conf0.mem_size = 1;
+  RMT.conf_ch[rmtChannel].conf0.carrier_en = 0;
+  RMT.conf_ch[rmtChannel].conf0.carrier_out_lv = 1;
+  RMT.conf_ch[rmtChannel].conf0.mem_pd = 0;
+ 
+  RMT.conf_ch[rmtChannel].conf1.rx_en = 0;
+  RMT.conf_ch[rmtChannel].conf1.mem_owner = 0;
+  RMT.conf_ch[rmtChannel].conf1.tx_conti_mode = 0;    //loop back mode.
+  RMT.conf_ch[rmtChannel].conf1.ref_always_on = 1;    // use apb clock: 80M
+  RMT.conf_ch[rmtChannel].conf1.idle_out_en = 1;
+  RMT.conf_ch[rmtChannel].conf1.idle_out_lv = 0;
+
+  return;
+}
+
+void ws2812_copy()
+{
+  unsigned int i, j, offset, len, bit;
+
+
+  offset = ws2812_half * MAX_PULSES;
+  ws2812_half = !ws2812_half;
+
+  len = (ws2812_len + 1) - ws2812_pos;
+  if (len > (MAX_PULSES / 8))
+    len = (MAX_PULSES / 8);
+  
+  for (i = 0; i < len; i++) {
+    if (i + ws2812_pos < ws2812_len) {
+      bit = ws2812_buffer[i + ws2812_pos];
+      for (j = 0; j < 8; j++, bit <<= 7)
+	RMTMEM.chan[RMTCHANNEL].data[j + i * 8 + offset].val =
+	  ws2812_bits[(bit >> 7) & 0x01].val;
+    }
+    else
+      RMTMEM.chan[RMTCHANNEL].data[i * 8 + offset].val = 0;
+  }
+
+  ws2812_pos += len;
+  return;
+}
+  
+void ws2812_handleInterrupt(void *arg)
+{
+  portBASE_TYPE taskAwoken = 0;
+
+
+  if (RMT.int_st.ch0_tx_thr_event) {
+    ws2812_copy();
+    if (ws2812_pos >= ws2812_len)
+      xSemaphoreGiveFromISR(ws2812_sem, &taskAwoken);
+    RMT.int_clr.ch0_tx_thr_event = 1;
+  }  
+
+  return;
+}
+
+void ws2812_init(int gpioNum)
+{
+  SET_PERI_REG_MASK(DPORT_PERIP_CLK_EN_REG, DPORT_RMT_CLK_EN);
+  CLEAR_PERI_REG_MASK(DPORT_PERIP_RST_EN_REG, DPORT_RMT_RST);
+
+  PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[gpioNum], 2);
+  gpio_matrix_out(gpioNum, RMT_SIG_OUT0_IDX + RMTCHANNEL, 0, 0);
+  gpio_set_direction(gpioNum, GPIO_MODE_OUTPUT);
+
+  ws2812_initRMTChannel(RMTCHANNEL);
+
+  RMT.tx_lim_ch[RMTCHANNEL].limit = MAX_PULSES;
+  intr_matrix_set(0, ETS_RMT_INTR_SOURCE, ETS_RMT_CTRL_INUM);
+  ESP_RMT_CTRL_INTRL(ws2812_handleInterrupt, NULL);
+  RMT.int_ena.ch0_tx_thr_event = 1;
+
+  ws2812_sem = xSemaphoreCreateBinary();
+
+  ws2812_bits[0].level0 = 1;
+  ws2812_bits[0].level1 = 0;
+  ws2812_bits[0].duration0 = ws2812_bits[0].duration1 = PULSE;
+  ws2812_bits[1].level0 = 1;
+  ws2812_bits[1].level1 = 0;
+  ws2812_bits[1].duration0 = ws2812_bits[2].duration1 = 2 * PULSE;
+  
+  ESP_RMT_CTRL_ENABLE();
+
+  return;
+}
+
+void ws2812_setColors(unsigned int length, rgbVal *array)
+{
+  unsigned int i;
+  
+
+  ws2812_len = (length * 3) * sizeof(uint8_t);
+  ws2812_buffer = malloc(ws2812_len);
+  
+  for (i = 0; i < length; i++) {
+    ws2812_buffer[0 + i * 3] = array[i].g;
+    ws2812_buffer[1 + i * 3] = array[i].r;
+    ws2812_buffer[2 + i * 3] = array[i].b;
+  }
+
+  ws2812_pos = 0;
+  ws2812_half = 0;
+
+  ws2812_copy();
+
+  if (ws2812_pos < ws2812_len)
+    ws2812_copy();
+  
+  RMT.conf_ch[RMTCHANNEL].conf1.mem_rd_rst = 1;
+  RMT.conf_ch[RMTCHANNEL].conf1.tx_start = 1;
+
+  xSemaphoreTake(ws2812_sem, portMAX_DELAY);
+  free(ws2812_buffer);
+  
+  return;
+}
